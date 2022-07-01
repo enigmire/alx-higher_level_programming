381a2bd334f57c3aebdba579f97da9f12493deb4
enigmire <meathical@gmail.com>
2022-07-01 04:51:43 -0700

    8. ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-    from magic_calculation_102 impor    if a < b:
+    if a < b:
         c = add(a, b)
         for loop in range(38, 98):
              loop = add(loop, i)
