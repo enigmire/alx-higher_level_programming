deb49846b47a8ab4a7a5c226ff458b5e977d1d9c
enigmire <meathical@gmail.com>
2022-07-04 06:27:31 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,4 +5,4 @@ def multiple_returns(sentence):
         if len(sentence) != 0:
             return (len(sentence), sentence[0])
         else:
-            return (sentence[0], None)
+            return (len(sentence), None)
