d52a9184952bdacc29315e64dd1f26541c5ebe7c
enigmire <meathical@gmail.com>
2022-07-20 13:07:13 -0700

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,6 +4,7 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
@@ -24,7 +25,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
-def area(self):
-    """Return the current area of the square."""
-    return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
